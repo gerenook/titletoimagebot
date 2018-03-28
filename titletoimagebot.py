e57515fa8dc0c69bb4123d69241ceb00b279a4b8
Dominik Simon <dodoo273@gmail.com>
2018-03-28 18:47:13 +0200

    rename, title splitting fix

--- titletoimagebot.py
+++ titletoimagebot.py
@@ -27,17 +27,17 @@ from prawcore.exceptions import ResponseException
 import apidata
 
 
-class Meme:
-    """Meme class
+class RedditImage:
+    """RedditImage class
 
-    :param image: the image (meme)
+    :param image: the image
     :type image: PIL.Image.Image
     """
     font_file = 'segoeui.ttf'
     font_scale_factor = 20
 
     def __init__(self, image):
-        self._meme = image
+        self._image = image
         self._width, self._height = image.size
 
     def _split_title(self, title):
@@ -65,27 +65,35 @@ class Meme:
             if character == delimiter:
                 new.append('')
                 i += 1
+        if not new[-1]:
+            new = new[:-1]
         if delimiter:
             return new
         return textwrap.wrap(title, 45)
 
-    def add_title(self, title):
-        """Add title to new whitespace on meme
+    def add_title(self, title, split=False):
+        """Add title to new whitespace on image
 
         :param title: the title to add
         :type title: str
+        :param split: if True, split title on [',', ';', '.'], else wrap text at 45 characters
+        :type split: bool
         """
-        font = ImageFont.truetype(Meme.font_file, self._width // Meme.font_scale_factor)
+        font = ImageFont.truetype(RedditImage.font_file,
+                                  self._width // RedditImage.font_scale_factor)
         line_height = font.getsize(title)[1]
-        texts = self._split_title(title)
-        whitespace_height = (line_height * len(texts)) + 10
+        if split:
+            texts = self._split_title(title)
+        else:
+            texts = textwrap.wrap(title, 45)
+        whitespace_height = (line_height * len(texts)) + 20
         new = Image.new('RGB', (self._width, self._height + whitespace_height), '#fff')
-        new.paste(self._meme, (0, whitespace_height))
+        new.paste(self._image, (0, whitespace_height))
         draw = ImageDraw.Draw(new)
         for i, text in enumerate(texts):
             draw.text((10, i * line_height), text, '#000', font)
         self._width, self._height = new.size
-        self._meme = new
+        self._image = new
 
     def add_author(self, author):
         """Add /u/author to top right of image
@@ -93,15 +101,16 @@ class Meme:
         :param author: the author to add (without /u/)
         :type author: str
         """
-        font = ImageFont.truetype(Meme.font_file, self._width // (Meme.font_scale_factor*2))
+        font = ImageFont.truetype(RedditImage.font_file,
+                                  self._width // (RedditImage.font_scale_factor*2))
         text = '/u/' + author
-        draw = ImageDraw.Draw(self._meme)
+        draw = ImageDraw.Draw(self._image)
         size = font.getsize(text)
         pos = (self._width - (size[0] + 10), 0)
         draw.text(pos, text, '#000', font)
 
     def upload(self, imgur):
-        """Upload self._meme to imgur
+        """Upload self._image to imgur
 
         :param imgur: the imgur api client
         :type imgur: imgurpython.client.ImgurClient
@@ -110,8 +119,8 @@ class Meme:
         """
         path_png = 'temp.png'
         path_jpg = 'temp.jpg'
-        self._meme.save(path_png)
-        self._meme.save(path_jpg)
+        self._image.save(path_png)
+        self._image.save(path_jpg)
         try:
             response = imgur.upload_from_path(path_png)
         except (ImgurClientError, ImgurClientRateLimitError) as error:
@@ -134,8 +143,6 @@ class TitleToImageBot:
     :type subreddit: str
     """
     def __init__(self, subreddit):
-        if subreddit != 'boottoobig':
-            raise NotImplementedError
         self._sql_connection = sqlite3.connect('database.db')
         self._sql = self._sql_connection.cursor()
         self._reddit = praw.Reddit(**apidata.reddit)
@@ -143,19 +150,22 @@ class TitleToImageBot:
         self._imgur = ImgurClient(**apidata.imgur)
         self._template = '[Image with title]({image_url})\n\n' \
                          '---\n\n' \
+                         '^^Summon ^^me ^^with ^^/u/TitleToImageBot ^^| ' \
                          '^^[remove](https://reddit.com/message/compose/' \
-                         '?to=TitleToMemeBot&subject=remove&message={comment_id}) ' \
+                         '?to=TitleToImageBot&subject=remove&message={comment_id}) ' \
                          '^^\\(for ^^OP\\) ^^| ' \
                          '^^[feedback](https://reddit.com/message/compose/' \
-                         '?to=TitleToMemeBot&subject=feedback) ^^| ' \
-                         '^^[source](https://github.com/gerenook/titletomemebot/' \
-                         'blob/master/titletomemebot.py)'
+                         '?to=TitleToImageBot&subject=feedback) ^^| ' \
+                         '^^[source](https://github.com/gerenook/titletoimagebot/' \
+                         'blob/master/titletoimagebot.py)'
 
-    def _process_submission(self, submission):
+    def _process_submission(self, submission, check_title):
         """Generate new image with added title and author, upload to imgur, reply to submission
 
         :param submission: the reddit submission object
-        :type submission: praw.models.reddit.submission.Submission
+        :type submission: praw.models.Submission
+        :param check_title: if True, check if title is part of rhyme
+        :type check_title: bool
         """
         # check db if submission was already processed
         author = submission.author.name
@@ -172,10 +182,11 @@ class TitleToImageBot:
         self._sql.execute('INSERT INTO submissions (id, author, title, url) VALUES (?, ?, ?, ?)',
                           params2)
         self._sql_connection.commit()
-        triggers = [',', '.', ';', 'roses']
-        if not any(t in title.lower() for t in triggers):
-            logging.info('Title is probably not part of rhyme, skipping submission')
-            return
+        if check_title:
+            triggers = [',', '.', ';', 'roses']
+            if not any(t in title.lower() for t in triggers):
+                logging.info('Title is probably not part of rhyme, skipping submission')
+                return
         if url.endswith('.gif') or url.endswith('.gifv'):
             logging.info('Image is animated gif, skipping submission')
             return
@@ -191,13 +202,13 @@ class TitleToImageBot:
             except OSError as error:
                 logging.error('Converting to image failed, skipping submission | %s', error)
                 return
-        meme = Meme(img)
+        image = RedditImage(img)
         logging.debug('Adding title and author')
-        meme.add_title(title)
-        meme.add_author(submission.author.name)
+        image.add_title(title, submission.subreddit.display_name == 'boottoobig')
+        image.add_author(submission.author.name)
         logging.debug('Trying to upload new image')
         for _ in range(3):
-            url = meme.upload(self._imgur)
+            url = image.upload(self._imgur)
             if url:
                 break
             logging.warning('Upload failed, retrying')
@@ -208,8 +219,13 @@ class TitleToImageBot:
         reply = self._template.format(image_url=url, comment_id='{comment_id}')
         try:
             comment = submission.reply(reply)
+        except praw.exceptions.APIException as rate_error:
+            logging.error('Ratelimit error, removing submission from database | %s', rate_error)
+            self._sql.execute('DELETE FROM submissions WHERE id=?', params1)
+            self._sql_connection.commit()
+            return
         except Exception as error:
-            logging.error('Cannot reply, skipping submission')
+            logging.error('Cannot reply, skipping submission | %s', error)
             return
         logging.debug('Editing comment with remove link')
         comment.edit(reply.format(comment_id=comment.id))
@@ -219,7 +235,7 @@ class TitleToImageBot:
         """Remove comment referenced in message body
 
         :param message: the remove message
-        :type message: praw.models.reddit.message.Message
+        :type message: praw.models.Message
         """
         comment_id = message.body
         logging.info('Found new remove message id:%s', comment_id)
@@ -243,11 +259,11 @@ class TitleToImageBot:
         """Forward message to creator
 
         :param message: the feedback message
-        :type message: praw.models.reddit.message.Message
+        :type message: praw.models.Message
         """
         message_author = message.author.name
         logging.info('Found new feedback message from %s', message_author)
-        subject = 'TitleToMemeBot feedback from ' + message_author
+        subject = 'TitleToImageBot feedback from ' + message_author
         body = message.body
         self._reddit.redditor(__author__).message(subject, body)
         message.mark_read()
@@ -256,8 +272,8 @@ class TitleToImageBot:
     def _process_message(self, message):
         """Process given message (remove, feedback, mark good/bad bot as read)
 
-        :param message: the inbox message
-        :type message: praw.models.reddit.message.Message
+        :param message: the inbox message, comment reply or username mention
+        :type message: praw.models.Message, praw.models.Comment
         """
         # check db if submission was already processed
         author = message.author.name
@@ -269,7 +285,7 @@ class TitleToImageBot:
         if self._sql.fetchone()[0]:
             logging.debug('Message %s found in database, returning', params1[0])
             return
-        logging.debug('New message: %s | %s', subject, body)
+        logging.debug('Message: %s | %s', subject, body)
         logging.debug('Adding message to database')
         self._sql.execute('INSERT INTO messages (id, author, subject, body) VALUES (?, ?, ?, ?)',
                           params2)
@@ -278,7 +294,11 @@ class TitleToImageBot:
         if author == self._reddit.user.me().name:
             logging.debug('Message was sent, returning')
             return
-        if subject == 'remove':
+        # process message
+        if subject == 'username mention' and isinstance(message, praw.models.Comment):
+            self._process_submission(message.submission, False)
+            message.mark_read()
+        elif subject == 'remove':
             self._process_remove_message(message)
         elif subject == 'feedback':
             self._process_feedback_message(message)
@@ -294,13 +314,13 @@ class TitleToImageBot:
 
         Process new submissions and messages
 
-        :param limit: amount of submission/messages to process
+        :param limit: amount of submissions/messages to process
         :type limit: int
         """
         logging.debug('Processing last %s submissions...', limit)
         for submission in self._subreddit.new(limit=limit):
-            self._process_submission(submission)
-        logging.debug('Processing last %s messages and/or comment replies...', limit)
+            self._process_submission(submission, True)
+        logging.debug('Processing last %s messages, comment replies or username mentions...', limit)
         for message in self._reddit.inbox.all(limit=limit):
             self._process_message(message)
 
@@ -314,7 +334,8 @@ def _setup_logging(level):
     :type level: int
     """
     console_handler = logging.StreamHandler()
-    file_handler = TimedRotatingFileHandler('./log/titletomemebot.log', when='midnight', interval=1)
+    file_handler = TimedRotatingFileHandler('./log/titletoimagebot.log',
+                                            when='midnight', interval=1)
     file_handler.suffix = '%Y-%m-%d'
     module_loggers = ['requests', 'urllib3', 'prawcore', 'PIL.Image', 'PIL.PngImagePlugin']
     for logger in module_loggers:
@@ -336,7 +357,7 @@ def _handle_exception(exc_type, exc_value, exc_traceback):
 def main():
     """Main function
 
-    Usage: ./titletomemebot.py [-h] limit interval
+    Usage: ./titletoimagebot.py [-h] limit interval
     """
     _setup_logging(logging.INFO)
     sys.excepthook = _handle_exception
@@ -351,7 +372,7 @@ def main():
         try:
             logging.info('Running bot')
             bot.run(args.limit)
-            logging.info('bot finished, restarting in %s seconds', args.interval)
+            logging.info('Bot finished, restarting in %s seconds', args.interval)
         except (requests.exceptions.ReadTimeout,
                 requests.exceptions.ConnectionError,
                 ResponseException):
